fa5e55d77db23ab90c7f678a3bd864bebbba4206
aihtnyc_h <feiver@naver.com>
2023-03-03 20:02:17 +0900

    fix: 게시글 좋아요 완료

--- src/main/java/instagram/instagrambe/post/controller/PostLikeController.java
+++ src/main/java/instagram/instagrambe/post/controller/PostLikeController.java
@@ -2,14 +2,7 @@ package instagram.instagrambe.post.controller;
 
 import instagram.instagrambe.config.UserDetailsImpl;
 import instagram.instagrambe.post.dto.PostResponseDto;
-import instagram.instagrambe.post.entity.Post;
-import instagram.instagrambe.post.entity.PostLike;
-import instagram.instagrambe.post.repository.PostLikeRepository;
-import instagram.instagrambe.post.repository.PostRepository;
 import instagram.instagrambe.post.service.PostLikeService;
-import instagram.instagrambe.post.service.PostService;
-import instagram.instagrambe.user.entity.User;
-import instagram.instagrambe.util.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -17,9 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import static instagram.instagrambe.util.ErrorCode.DUPLICATE_RESOURCE;
-import static instagram.instagrambe.util.ErrorCode.NOT_FOUND_DATA;
 
 @RestController
 @RequiredArgsConstructor
