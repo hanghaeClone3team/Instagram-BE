3fff8f854fa260a4e3db764f75049e5998b719f0
aihtnyc_h <feiver@naver.com>
2023-03-03 20:56:15 +0900

    fix: 게시글에 댓글 추가 CommentRepository 수정

--- src/main/java/instagram/instagrambe/comment/repository/CommentRepository.java
+++ src/main/java/instagram/instagrambe/comment/repository/CommentRepository.java
@@ -1,7 +1,12 @@
 package instagram.instagrambe.comment.repository;
 
 import instagram.instagrambe.comment.entity.Comment;
+import instagram.instagrambe.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findAllByBlogOrderByCreateAtDesc(Post post);
+    //void deleteAllByBlog(Post post);
 }
