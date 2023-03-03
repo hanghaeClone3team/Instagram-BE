6e8c7d5b523b4f636e88fe9fe54b48d617e6867e
aihtnyc_h <feiver@naver.com>
2023-03-03 19:45:58 +0900

    feat: 게시글 좋아요 완성

--- src/main/java/instagram/instagrambe/post/controller/PostLikeController.java
+++ src/main/java/instagram/instagrambe/post/controller/PostLikeController.java
@@ -1,11 +1,33 @@
 package instagram.instagrambe.post.controller;
 
+import instagram.instagrambe.config.UserDetailsImpl;
+import instagram.instagrambe.post.dto.PostResponseDto;
+import instagram.instagrambe.post.entity.Post;
+import instagram.instagrambe.post.entity.PostLike;
+import instagram.instagrambe.post.repository.PostLikeRepository;
+import instagram.instagrambe.post.repository.PostRepository;
+import instagram.instagrambe.post.service.PostLikeService;
+import instagram.instagrambe.post.service.PostService;
+import instagram.instagrambe.user.entity.User;
+import instagram.instagrambe.util.CustomException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static instagram.instagrambe.util.ErrorCode.DUPLICATE_RESOURCE;
+import static instagram.instagrambe.util.ErrorCode.NOT_FOUND_DATA;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/content")
 public class PostLikeController {
+   private final PostLikeService postLikeService;
+   @PostMapping("/{post_id}/like")
+   public ResponseEntity<PostResponseDto> likePost(@PathVariable Long post_id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+       return postLikeService.LikePost(post_id, userDetails.getUser());
+   }
 }
--- src/main/java/instagram/instagrambe/post/service/PostLikeService.java
+++ src/main/java/instagram/instagrambe/post/service/PostLikeService.java
@@ -1,9 +1,45 @@
 package instagram.instagrambe.post.service;
 
+import instagram.instagrambe.post.dto.PostResponseDto;
+import instagram.instagrambe.post.entity.Post;
+import instagram.instagrambe.post.entity.PostLike;
+import instagram.instagrambe.post.repository.PostLikeRepository;
+import instagram.instagrambe.post.repository.PostRepository;
+import instagram.instagrambe.user.entity.User;
+import instagram.instagrambe.util.CustomException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import static instagram.instagrambe.util.ErrorCode.DUPLICATE_RESOURCE;
+import static instagram.instagrambe.util.ErrorCode.NOT_FOUND_DATA;
 
 @Service
 @RequiredArgsConstructor
 public class PostLikeService {
+    private final PostRepository postRepository;
+    private final PostLikeRepository postLikeRepository;
+    private final PostService postService;
+
+    public ResponseEntity<PostResponseDto> LikePost(Long post_id, User user) {
+        Post post = postRepository.findById(post_id).orElseThrow(
+                () -> new CustomException(NOT_FOUND_DATA));
+        Long likeCheck = postLikeRepository.countByPost_IdAndUser_Id(post_id, user.getId());
+        boolean heart = false;
+        if(likeCheck == 0){ //좋아용
+            heart = true;
+            System.out.println("00000heart = " + likeCheck);
+            postService.likePost(post, heart);
+            PostLike like = postLikeRepository.saveAndFlush(new PostLike(post,user));
+        }else if(likeCheck ==1) { //좋아요
+            System.out.println("11111heart = " + likeCheck);
+            postService.likePost(post, heart);
+            postLikeRepository.deleteByPost_IdAndUser_Id(post_id, user.getId());
+        }else {
+            System.out.println("-----likeCheck = " + likeCheck);
+            throw new CustomException(DUPLICATE_RESOURCE);
+        }
+
+        return ResponseEntity.ok().body(PostResponseDto.of(post));
+    }
 }
