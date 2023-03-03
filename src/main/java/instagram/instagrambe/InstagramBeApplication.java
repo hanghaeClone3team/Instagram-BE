90d8bd152c4b5bc8dfd8721cd34d2d3ef0e15017
SeongMinnnn <122071252+SeongMinnnn@users.noreply.github.com>
2023-03-03 15:29:39 +0900

    시작

---
+++ src/main/java/instagram/instagrambe/InstagramBeApplication.java
@@ -0,0 +1,13 @@
+package instagram.instagrambe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstagramBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/instagram/instagrambe/InstagramBeApplicationTests.java
@@ -0,0 +1,13 @@
+package instagram.instagrambe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
