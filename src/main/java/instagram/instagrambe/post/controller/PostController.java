fa176670b6aa696bf626bad9ed735628ddd943f1
aihtnyc_h <feiver@naver.com>
2023-03-03 19:58:50 +0900

    feat: 게시글 전체조회 완성

--- src/main/java/instagram/instagrambe/post/controller/PostController.java
+++ src/main/java/instagram/instagrambe/post/controller/PostController.java
@@ -7,12 +7,14 @@ import instagram.instagrambe.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,7 +26,16 @@ public class PostController {
     public ResponseEntity<PostResponseDto> createPost(@RequestPart MultipartFile image,
                                                       @RequestPart PostRequestDto postRequestDto,
                                                       @AuthenticationPrincipal UserDetailsImpl userDetails) throws IOException {
+        System.out.println("-------image.getOriginalFilename() = " + image.getOriginalFilename());
+        System.out.println("-------image.getContentType() = " + image.getContentType());
+        System.out.println("-------requestDtoContents = " + postRequestDto.getContents());
     return postService.createPost(image, postRequestDto, userDetails.getUser());
+    }
+
+    // 게시글 전체 조회
+    @GetMapping
+    public ResponseEntity<List<PostResponseDto>> getBlogs(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return postService.getPosts(userDetails.getUser());
     }
 }
 
