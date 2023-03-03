5544b0863300ecf22de9eef8fbbc3bf8f9fa8931
SeongMinnnn <122071252+SeongMinnnn@users.noreply.github.com>
2023-03-03 20:53:33 +0900

    feat: 로그아웃 확인

--- src/main/java/instagram/instagrambe/user/controller/UserController.java
+++ src/main/java/instagram/instagrambe/user/controller/UserController.java
@@ -12,15 +12,15 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
-import java.util.Optional;
 
 @Getter
 @RestController
@@ -47,5 +47,14 @@ public class UserController {
     public ResponseEntity<String> login(@RequestBody LoginRequestDto requestDto, HttpServletResponse response){
         userService.login(requestDto, response);
         return ResponseEntity.status(HttpStatus.OK).body("로그인 완료");
+    }
+
+    @GetMapping("/logout")
+    public ResponseEntity<String> logout(HttpServletRequest request, HttpServletResponse response){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body("로그아웃 완료");
     }
 }
