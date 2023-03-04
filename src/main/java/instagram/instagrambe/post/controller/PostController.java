0e56ec5c9ccafeb6cf28628a62a926572c6cbdb2
aihtnyc_h <feiver@naver.com>
2023-03-04 13:42:54 +0900

    fix: 게시글 api 주소 수정

--- src/main/java/instagram/instagrambe/post/controller/PostController.java
+++ src/main/java/instagram/instagrambe/post/controller/PostController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/content")
+@RequestMapping("/api/post")
 public class PostController {
     private final PostService postService;
 
