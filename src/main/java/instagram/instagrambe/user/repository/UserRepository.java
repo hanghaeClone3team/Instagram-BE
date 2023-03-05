28f245a97a67f9570c61fd889a96990466cf3cb1
SeongMinnnn <122071252+SeongMinnnn@users.noreply.github.com>
2023-03-05 20:00:57 +0900

    Revert "feat: 카카오 로그인 추가"

--- src/main/java/instagram/instagrambe/user/repository/UserRepository.java
+++ src/main/java/instagram/instagrambe/user/repository/UserRepository.java
@@ -8,5 +8,4 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
-    Optional<User> findByKaKaoId(Long id);
 }
