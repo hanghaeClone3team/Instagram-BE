c607c541f8725e7d1bcdd08ced96f47543ecd66c
SeongMinnnn <122071252+SeongMinnnn@users.noreply.github.com>
2023-03-06 17:55:00 +0900

    fix: 팔로우 수정 및 카카오 구현

--- src/main/java/instagram/instagrambe/follow/repository/FollowRepository.java
+++ src/main/java/instagram/instagrambe/follow/repository/FollowRepository.java
@@ -1,8 +1,11 @@
 package instagram.instagrambe.follow.repository;
 
 import instagram.instagrambe.follow.entity.Follow;
+import instagram.instagrambe.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FollowRepository extends JpaRepository<Follow, Long> {
+public interface FollowRepository extends JpaRepository<Follow, User> {
 
+    Follow findByFollowerAndFollowing(User follower, User following);
+    Follow deleteByFollowerAndFollowing(User follower, User following);
 }
